df57222bcd1423e05cb950497ed6119fa00a89d8
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-28 17:14:54 +0545

    register urls for hospitals and staff management

--- apps/hospitals/urls.py
+++ apps/hospitals/urls.py
@@ -1,4 +1,18 @@
 from django.urls import path
+from apps.hospitals.views.hospital import (
+    HospitalListCreateView,
+    HospitalDetailView,
+    HospitalStatusView,
+)
+from apps.hospitals.views.staff import StaffListAddView, StaffDetailView
 
 urlpatterns = [
+    # Hospital 
+    path("", HospitalListCreateView.as_view(), name="hospital-list-create"),
+    path("<uuid:hospital_id>/", HospitalDetailView.as_view(), name="hospital-detail"),
+    path("<uuid:hospital_id>/status/", HospitalStatusView.as_view(), name="hospital-status"),
+
+    # Staff management 
+    path("<uuid:hospital_id>/staff/", StaffListAddView.as_view(), name="staff-list-add"),
+    path("<uuid:hospital_id>/staff/<uuid:membership_id>/", StaffDetailView.as_view(), name="staff-detail"),
 ]
