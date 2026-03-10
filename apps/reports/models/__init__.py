b6647ee681c0c880fa4191654e537477aeb5d611
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-03-10 17:24:00 +0545

    import all report's models

---
+++ apps/reports/models/__init__.py
@@ -0,0 +1,6 @@
+from .report import Report
+from .report_version import ReportVersion
+from .report_template import ReportTemplate, TemplateField
+from .report_section import ReportSection
+
+__all__ = ["Report", "ReportVersion", "ReportTemplate", "TemplateField", "ReportSection"]
