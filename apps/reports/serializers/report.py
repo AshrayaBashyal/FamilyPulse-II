20c2f286427fa1c7e10b5ebb44e210da95a30217
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-03-12 19:02:02 +0545

    fix typo and add serializer for creating report_template and reading all fields of template

--- apps/reports/serializers/report.py
+++ apps/reports/serializers/report.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from apps.reports.models import Report, ReportVersion, ReportTemplate, TemplateField, ReportSection
 
 
-class TemplateFieldSeriailzer(serializers.ModelSerializer):
+class TemplateFieldSerializer(serializers.ModelSerializer):
     choices = serializers.ListField(
         child = serializers.CharField,
         required = False,
@@ -15,4 +15,31 @@ class TemplateFieldSeriailzer(serializers.ModelSerializer):
             "id", "name", "label", "field_type",
             "required", "choices", "order", "help_text",
         ]
+        read_only_fields = ["id"]
+
+
+class CreateReportTemplateSerializer(serializers.Serializer):     # class CreateReportTemplateSerializer(serializers.ModelSerializer)??
+    """
+    Used when hospital admin creates a template.
+    """
+    # visit_type = serializers.PrimaryKeyRelatedField(
+    #     queryset=__import__(
+    #         "apps.visits.models", fromlist=["VisitType"]
+    #     ).VisitType.objects.all()
+    # )
+    from apps.visits.models import VisitType
+
+    visit_type = serializers.PrimaryKeyRelatedField(
+    queryset=VisitType.objects.all()
+    )
+    description = serializers.CharField(required=False, allow_blank=True, default="")
+ 
+
+class ReportTemplateSerializer(serializers.ModelSerializer):
+    """Read-only full template with all fields. Used for GET."""
+    fields = TemplateFieldSerializer(many=True, read_only=True)
+ 
+    class Meta:
+        model = ReportTemplate
+        fields = ["id", "visit_type", "description", "fields"]
         read_only_fields = ["id"]
